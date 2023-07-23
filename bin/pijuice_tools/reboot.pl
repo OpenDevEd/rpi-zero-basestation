0913d28234d7441479c440548de30add08e3feef
bjohas <bjohas+git@gmail.com>
2023-07-23 10:59:54 +0100

    Updated reboot scripts
    reboot.pl will now check whether the alarm is set properly, and only if not set properly, will reset it.
    
    reboot.py: added lowpower function for shut down on low power

--- bin/pijuice_tools/reboot.pl
+++ bin/pijuice_tools/reboot.pl
@@ -7,6 +7,8 @@ use feature qw{ say signatures }; no warnings qw{ experimental::signatures };
 #use File::Slurper qw(read_text read_lines write_text);
 my $home = $ENV{HOME};
 chomp(my $date = `date +'%Y-%m-%d_%H.%M.%S'`);
+chomp(my $hour = `date +'%H'`);
+chomp(my $minute = `date +'%M'`);
 my $hasargs = $#ARGV;
 my $help = "";
 my $string = "";
@@ -24,7 +26,22 @@ sub main() {
     my $uptime = `cat /proc/uptime`;
     $uptime =~ s/ .*//;    
     if ($uptime < 5 * 60) {
-	system("/usr/bin/python3 /home/ilce/bin/pijuice_tools/reboot.py reset");
+	my $target = "Alarm:  {'second': 0, 'minute': 0, 'hour': 6, 'day': 'EVERY_DAY'}
+Controlstatus:  {'alarm_wakeup_enabled': True, 'alarm_flag': True}
+";
+	my $alarm = `bin/utils/pijuice_util.py --get-alarm`;
+	if ($alarm ne $target) {
+	    say $alarm;
+	    say $target;
+	    system("/usr/bin/python3 /home/ilce/bin/pijuice_tools/reboot.py reset");
+	} else {
+	    say "Alarm set properly.";
+	};
+    };
+    if ($uptime < 60 * 60 && $hour eq "06") {
+	say "Booted and after 6:00? Yes.";
+    } else {
+	say "Booted and after 6:00? No.";
     };
 };
  
--- bin/pijuice_tools/reboot.py
+++ bin/pijuice_tools/reboot.py
@@ -49,15 +49,43 @@ def reboot_pi():
     print("Rebooting the pi...")
     os.system("sudo reboot now")
 
+def lowpower(threshold, turnon):
+    power = pijuice.status.GetChargeLevel()
+    print("Current power level: "+str(power["data"]))
+    indicator = power["data"] <= threshold
+    print("Low power condition met? "+str(indicator))
+    wac = pijuice.power.GetWakeUpOnCharge()
+    if wac["data"] != turnon: 
+        print("Previous wakeup setting: "+str(wac))
+        print(pijuice.power.SetWakeUpOnCharge(turnon))
+        wac = pijuice.power.GetWakeUpOnCharge()
+        print("New wakeup setting: "+str(wac))
+    else:
+        print("Wakup set to: "+str(wac["data"]))
+    return indicator
+
 if len(sys.argv)<2:
     print("provide arguments")
     quit()
 
 if sys.argv[1] == "halt":
-    print("Shutting down now. Disconnecting power in 60 seconds.")
+    print("Shutting down now. Disconnecting power in 60 seconds. Scheduled wakeup only.")
     #pijuice.status.SetLedBlink('D2', 60, [200,0,0], 500, [0, 0, 0], 500)
+    reset_to_defaults()
     turn_off_pijuice(60)
     turn_off_pi()
+if sys.argv[1] == "lowbattery":
+    if len(sys.argv)<4:
+        print("provide arguments")
+        quit()
+    if lowpower(int(sys.argv[2]), int(sys.argv[3])):
+        print("Low power: Shutting down now. Disconnecting power in 60 seconds. Scheduled wakeup only. Charge wakeup enabled.")
+        #pijuice.status.SetLedBlink('D2', 60, [200,0,0], 500, [0, 0, 0], 500)
+        reset_to_defaults()
+        turn_off_pijuice(60)
+        turn_off_pi()
+    else: 
+        print("Power level ok, not doing anything.")
 elif sys.argv[1] == "reboot":
     print("Restarting in approx 70 seconds...")
     #pijuice.status.SetLedBlink('D2', 70, [0,200,200], 200, [0, 0, 0], 800)
@@ -67,10 +95,12 @@ elif sys.argv[1] == "reboot":
 elif sys.argv[1] == "poff":
     print("Pulling power in 200 seconds...")
     #pijuice.status.SetLedBlink('D2', 200, [200,0,0], 500, [0, 0, 0], 500)
+    reset_to_defaults()
     turn_off_pijuice(200)
 elif sys.argv[1] == "reset":
     reset_to_defaults()
 elif sys.argv[1] == "resetonboot":
+    # This isn't working in crontab. see workaround in reboot.pl
     #pijuice.status.SetLedBlink('D2', 20, [200,0,200], 250, [0, 0, 0], 250)
     time.sleep(10)
     reset_to_defaults()
