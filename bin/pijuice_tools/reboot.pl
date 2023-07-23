170ba3edc1cb79913e01c31269efce7c25a75462
bjohas <bjohas+git@gmail.com>
2023-07-23 13:28:50 +0100

    Introduced settings for reboot conditions (time and power)

--- bin/pijuice_tools/reboot.pl
+++ bin/pijuice_tools/reboot.pl
@@ -1,55 +1,134 @@
 #!/usr/bin/perl
-use warnings; use strict; use utf8;
+use warnings;
+use strict;
+use utf8;
 use open IO => ':encoding(UTF-8)', ':std';
-use feature qw{ say signatures }; no warnings qw{ experimental::signatures };
-# use experimental 'smartmatch'; use experimental qw(declared_refs);
-# use 5.34.0; use experimental qw{ try }; no warnings { experimental::try };
-#use File::Slurper qw(read_text read_lines write_text);
+use feature qw{ say signatures };
+use JSON qw( decode_json  encode_json to_json from_json);
+
+no warnings qw{ experimental::signatures };
 my $home = $ENV{HOME};
-chomp(my $date = `date +'%Y-%m-%d_%H.%M.%S'`);
-chomp(my $hour = `date +'%H'`);
-chomp(my $minute = `date +'%M'`);
+chomp( my $date   = `date +'%Y-%m-%d_%H.%M.%S'` );
+chomp( my $hour   = `date +'%H'` );
+chomp( my $minute = `date +'%M'` );
 my $hasargs = $#ARGV;
-my $help = "";
-my $string = "";
-my $number = "";
+my $help    = "";
+my $string  = "";
+my $set     = "";
 use Getopt::Long;
-GetOptions (
-    "string=s" => \$string, 
-    "help" => \$help, 
-    "number=f" => \$number, 
+my $force = "";
+my $force2 = "";
+GetOptions(
+    "string=s" => \$string,
+    "help"     => \$help,
+    "set"      => \$set,
+    "force" => \$force,
+    "force2" => \$force2,
     ) or die("Error in command line arguments\n");
+
+
+my %settings = %{from_json(`cat /home/ilce/bin/pijuice_tools/settings.json`)};
+
+if ($set) {
+
+    # For testing...
+    if ( !$hasargs ) {
+        say
+"Usage: $0 --set hour_for_wakeup minute_forwakeup battery_threshold_for_shutdown battery_threshold_for_turnon";
+say "Current settings:";
+say `/usr/bin/python3 /home/ilce/bin/pijuice_tools/reboot.py show`;    
+
+    }
+    else {
+        system
+"/usr/bin/python3 /home/ilce/bin/pijuice_tools/reboot.py reset $ARGV[0] $ARGV[1] $ARGV[2] $ARGV[3]";    
+system "/usr/bin/python3 /home/ilce/bin/pijuice_tools/reboot.py lowbattery $ARGV[2] $ARGV[3]";    
+system "/usr/bin/python3 /home/ilce/bin/pijuice_tools/reboot.py show";    
+    say "reboot.pl: If you have crontab set up, the above settings might be overridden if you run them near a crontab job execution.";
+    say `date`;
+    };
+    exit;
+}
+
+my $wakehour = $settings{"default_hour"};
+my $wakeminute = $settings{"default_minute"};
+my $padhour  = $wakehour;
+my $padminute  = $wakeminute;
+if ( length($padhour) < 2 ) { $padhour = "0$padhour"; }
+if ( length($padminute) < 2 ) { $padminute = "0$padminute"; }
 
 &main();
 
 sub main() {
     my $uptime = `cat /proc/uptime`;
-    $uptime =~ s/ .*//;    
-    if ($uptime < 5 * 60) {
-	my $target = "Alarm:  {'second': 0, 'minute': 0, 'hour': 6, 'day': 'EVERY_DAY'}
+    $uptime =~ s/ .*//;
+    if ( $uptime < 5 * 60 || $force) {
+# We are within 5 minutes of boot. Make sure that the wakeup alam and battery is set properly.
+        my $target =
+"Alarm:  {'second': 0, 'minute': $wakeminute, 'hour': $wakehour, 'day': 'EVERY_DAY'}
 Controlstatus:  {'alarm_wakeup_enabled': True, 'alarm_flag': True}
 ";
-	my $alarm = `bin/utils/pijuice_util.py --get-alarm`;
-	if ($alarm ne $target) {
-	    say $alarm;
-	    say $target;
-	    system("/usr/bin/python3 /home/ilce/bin/pijuice_tools/reboot.py reset");
-	} else {
-	    say "Alarm set properly.";
-	};
-    };
-    if ($uptime < 60 * 60 && $hour eq "06") {
-	say "Booted and after 6:00? Yes.";
-    } else {
-	say "Booted and after 6:00? No.";
-    };
-};
- 
-__END__
-    #use String::ShellQuote; $string = shell_quote(@list);
-    #use Data::Dumper;
-    #use JSON qw( decode_json  encode_json to_json from_json);
-    #use Encode;
-    #use Try::Tiny;
-    # Path::Tiny; IO::All
-    # my ($volume,$directories,$file) = File::Spec->splitpath( $path );
+        my $alarm = `bin/utils/pijuice_util.py --get-alarm`;
+        if ( $alarm ne $target ) {
+            say $alarm;
+            say $target;
+            system(
+"/usr/bin/python3 /home/ilce/bin/pijuice_tools/reboot.py reset $wakehour $wakeminute $settings{default_lowpower_shutoff} $settings{default_chargelevel_turnon}"
+            );
+            make_entry("reboot.pl: Setting up alarm/battery.");
+        }
+        else {
+            say "reboot.pl: Alarm set properly.";
+            make_entry("reboot.pl: Alarm set properly/battery.");
+        }
+    }
+    my $minutes = 15;
+    print "reboot.pl: Within $minutes minutes of boot?";
+    if ( $uptime < $minutes * 60 || $force2) {
+        say "Yes.";
+        make_entry(
+"reboot.pl: We're witin $minutes minutes of boot. Will not shut down due to low battery."
+        );
+        print "Are we between $hour:00 and $hour:15? ";
+        if ( $hour eq $padhour ) {
+            say "Yes.";
+#            if ( $uptime < 5 * 60 ) {
+#                make_entry(
+#"reboot.pl: Woken up just after $padhour:00; not applying low power condition until $padhour:$minutes"
+#                );
+#            }
+        }
+        else {
+            say "No.";
+        }
+    }
+    else {
+        say "No.";
+      # We're outside of 15 minutes of boot, so we will now shut down if needed.
+        my $lowpower =
+          `/usr/bin/python3 /home/ilce/bin/pijuice_tools/reboot.py lowbattery`;
+        if ( $lowpower =~ m/lowpowercondition\=(\w+)/ ) {
+            say "reboot.pl: Low power condition = $1";
+            if ( $1 eq "True" ) {
+                make_entry("reboot.pl: Low power condition $1.");
+            }
+        }
+        else {
+            make_entry(
+                "reboot.pl: ERROR - could not determine low power condition.");
+        }
+    }
+}
+
+sub make_entry($text) {
+    chomp( my $path = `date +'%Y-%m-%d/%H'` );
+    print "$path\n";
+    my $location = $home . "/logs/" . $path;
+    print "$location\n";
+    system( "mkdir", "-p", $location ) if !-d $location;
+    my $date = `date --iso=sec`;
+    open F, ">>$location/bootlog.txt";
+    print F $text . "\t" . $date;
+    print $text . "\t" . $date;
+    close F;
+}
--- bin/pijuice_tools/reboot.py
+++ bin/pijuice_tools/reboot.py
@@ -1,10 +1,30 @@
 #!/usr/bin/python3
 #Perform a guaranteed reboot and must be run with sudo
+import json
 from pijuice import PiJuice # Import pijuice module
 import os
 import sys
 from datetime import datetime , timedelta
 import time
+
+# PiJuice settings:
+default_hour = 6
+default_minute = 0
+default_chargelevel_turnon = 30
+# Note: If the default_alarm time is changed, then reboot.pl may also needs to be updated.
+
+# Script settings:
+default_lowpower_shutoff = 20
+
+# read default_hour, default_minute, default_chargelevel_turnon, default_lowpower_shutoff from json file
+with open('/home/ilce/bin/pijuice_tools/settings.json', 'r') as f:
+    settings = json.load(f)
+    default_hour = settings['default_hour']
+    default_minute = settings['default_minute']
+    default_chargelevel_turnon = settings['default_chargelevel_turnon']
+    default_lowpower_shutoff = settings['default_lowpower_shutoff']
+
+default_alarm =  {'second': 0, 'minute': default_minute, 'hour': default_hour, 'day': 'EVERY_DAY'}
 
 # Since the start is very early in the boot sequence we wait for the i2c-1 device
 while not os.path.exists('/dev/i2c-1'):
@@ -13,8 +33,11 @@ while not os.path.exists('/dev/i2c-1'):
 pijuice = PiJuice(1, 0x14) # Instantiate PiJuice interface object
 
 def show_alarm():
-    print(pijuice.rtcAlarm.GetAlarm())
-    print(pijuice.power.GetPowerOff())
+    print("rtcAlarm.GetAlarm="+str(pijuice.rtcAlarm.GetAlarm()))
+    print("power.GetPowerOff (time to power off)="+str(pijuice.power.GetPowerOff()))
+    wac = pijuice.power.GetWakeUpOnCharge()
+    print("wakeup.GetWakeUpOnCharge="+str(wac))
+    print("Script property: default_lowpower_shutoff="+str(default_lowpower_shutoff))
 
 def set_wakeup_in_x_sec(x):
     #print(pijuice.rtcAlarm.GetAlarm());
@@ -25,10 +48,12 @@ def set_wakeup_in_x_sec(x):
     pijuice.rtcAlarm.SetWakeupEnabled(True)
     pijuice.rtcAlarm.SetAlarm(arr)
 
-def reset_to_defaults():
-    arr =  {'second': 0, 'minute': 0, 'hour': 6, 'day': 'EVERY_DAY'}
+def reset_to_defaults(hour=default_hour, minute=default_minute):
     pijuice.rtcAlarm.SetWakeupEnabled(True)
-    pijuice.rtcAlarm.SetAlarm(arr)
+    myalarm = default_alarm
+    myalarm['hour'] = hour
+    myalarm['minute'] = minute
+    pijuice.rtcAlarm.SetAlarm(myalarm)
     pijuice.power.SetPowerOff(-1)
     # Wakeup on charge at 50%
 
@@ -52,12 +77,15 @@ def reboot_pi():
 def lowpower(threshold, turnon):
     power = pijuice.status.GetChargeLevel()
     print("Current power level: "+str(power["data"]))
+    print("Shutdown threshold: "+str(threshold))
     indicator = power["data"] <= threshold
     print("Low power condition met? "+str(indicator))
+    # The following string is matched in reboot.pl. Don't change it.
+    print("lowpowercondition="+str(indicator))
     wac = pijuice.power.GetWakeUpOnCharge()
     if wac["data"] != turnon: 
         print("Previous wakeup setting: "+str(wac))
-        print(pijuice.power.SetWakeUpOnCharge(turnon))
+        print(pijuice.power.SetWakeUpOnCharge(turnon, non_volatile = True))
         wac = pijuice.power.GetWakeUpOnCharge()
         print("New wakeup setting: "+str(wac))
     else:
@@ -74,13 +102,18 @@ if sys.argv[1] == "halt":
     reset_to_defaults()
     turn_off_pijuice(60)
     turn_off_pi()
-if sys.argv[1] == "lowbattery":
-    if len(sys.argv)<4:
-        print("provide arguments")
-        quit()
-    if lowpower(int(sys.argv[2]), int(sys.argv[3])):
+if sys.argv[1] == "lowbattery" or sys.argv[1] == "lowpower":
+    if len(sys.argv) >= 3:
+        print("Setting low power threshold to: "+str(sys.argv[2]))
+        lowpower_shutoff = int(sys.argv[2])
+    else: 
+        lowpower_shutoff = default_lowpower_shutoff
+    if len(sys.argv) >= 4:
+        chargelevel_turnon = int(sys.argv[3])
+    else:
+        chargelevel_turnon = default_chargelevel_turnon
+    if lowpower(lowpower_shutoff, chargelevel_turnon):
         print("Low power: Shutting down now. Disconnecting power in 60 seconds. Scheduled wakeup only. Charge wakeup enabled.")
-        #pijuice.status.SetLedBlink('D2', 60, [200,0,0], 500, [0, 0, 0], 500)
         reset_to_defaults()
         turn_off_pijuice(60)
         turn_off_pi()
@@ -98,7 +131,21 @@ elif sys.argv[1] == "poff":
     reset_to_defaults()
     turn_off_pijuice(200)
 elif sys.argv[1] == "reset":
-    reset_to_defaults()
+    hour = default_hour
+    if len(sys.argv) >= 3:
+        hour = int(sys.argv[2])
+    minute = default_minute
+    if len(sys.argv) >= 4:
+        minute = int(sys.argv[3])
+    lowpower_shutoff = default_lowpower_shutoff
+    if len(sys.argv) >= 5:
+        print("Setting low power threshold to: "+str(sys.argv[4]))
+        lowpower_shutoff = int(sys.argv[4])
+    chargelevel_turnon = default_chargelevel_turnon
+    if len(sys.argv) >= 6:
+        chargelevel_turnon = int(sys.argv[5])
+    reset_to_defaults(hour, minute)
+    lowpower(lowpower_shutoff, chargelevel_turnon)
 elif sys.argv[1] == "resetonboot":
     # This isn't working in crontab. see workaround in reboot.pl
     #pijuice.status.SetLedBlink('D2', 20, [200,0,200], 250, [0, 0, 0], 250)
@@ -107,5 +154,5 @@ elif sys.argv[1] == "resetonboot":
 elif sys.argv[1] == "show":
     show_alarm()
 else:
-    print("Specify halt or reboot or reset")
+    print("Specify halt or reboot or reset or lowbattery")
 
