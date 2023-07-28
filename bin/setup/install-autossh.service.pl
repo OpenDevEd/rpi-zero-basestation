87462db0933376a2bab39cb98d1eedb23a843978
bjohas <bjohas+git@gmail.com>
2023-07-28 16:24:03 +0100

    Update install-autossh.service.pl

--- bin/setup/install-autossh.service.pl
+++ bin/setup/install-autossh.service.pl
@@ -4,7 +4,7 @@ use warnings; use strict; use utf8;
 my $login = (getpwuid $>);
 die "must run as root" if $login ne 'root';
 
-chomp($remotehost = `ssh root@157.245.35.62 "hostname"`);
+chomp(my $remotehost = `ssh root@157.245.35.62 "hostname"`);
 if ($remotehost eq "ssh-tunnel-exchange-point") {
 } else {
     print("This will only work if you have the ssh keys exchanged properly. Check your keys.");
@@ -20,9 +20,9 @@ chomp(my $host = `hostname`);
 if ($host =~ m/(\d+)/) {
     my $number = $1;
     while (length($number) < 3) {
-	$number = "0$nunber";
+	$number = "0$number";
     };
-    $number = "22$nunber";
+    $number = "22$number";
     $text =~ s/\<PORT\>/$number/s;
 
     print "Setting up autossh service connecting to port $number\n";
