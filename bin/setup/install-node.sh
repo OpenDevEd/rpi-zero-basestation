26968c6be137afc8567571c84881cb214d726fc3
bjohas <bjohas+git@gmail.com>
2023-08-07 18:12:39 +0100

    add installation scripts for node and pm2

---
+++ bin/setup/install-node.sh
@@ -0,0 +1,6 @@
+# zigbee2mqtt needs node v 18
+wget -O - https://raw.githubusercontent.com/audstanley/NodeJs-Raspberry-Pi/master/Install-Node.sh | sudo bash;
+node -v;
+sudo node-install -v 18;
+node --version
+npm --version
