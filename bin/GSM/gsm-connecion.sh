9f6d998fbf19f5ab2e9b1cf748f544b2bcd0cb51
OpenDevEd <github@opendeved.net>
2023-08-28 23:31:18 +0100

    update

--- bin/GSM/gsm-connecion.sh
+++ bin/GSM/gsm-connecion.sh
@@ -1,19 +1,25 @@
-#!/usr/bin/bash
+#!/usr/bin/bash -x
+
+cd ~/rpi-zero-basestation/bin/GSM/
 
 if [[ $1 = "on" ]]
 then
+    ./phat-gsm_on_off.py on
     sudo pon gsm.mm
+    sudo pppd call gsm.mm
 elif [[ $1 = "off" ]]
 then
     sudo poff gsm.mm    
+    ./phat-gsm_on_off.py off
 elif [[ $1 = "ip" ]]
 then
     ip a
-elif [[ $1 = "check" ]]
+elif [[ $1 = "test" ]]
 then
-    # test
+    echo Testing ping, please wait
     ping -c 3 -I ppp0 -n google.com
+    echo Testing https - must be run with sudo, please wait
     sudo curl -v -4 --interface ppp0 https://google.com
 else
-    echo "Valid options: on off ip check"
+    echo "Valid options: on off ip test"
 fi
--- bin/GSM/test_signal_strength.sh
+++ bin/GSM/test_signal_strength.sh
@@ -1,5 +1,6 @@
 #!/bin/sh -x
 sudo poff
 ./phat-gsm_on_off.py on
-./send_sms.py
+./signal-strength.py
 ./phat-gsm_on_off.py off
+
